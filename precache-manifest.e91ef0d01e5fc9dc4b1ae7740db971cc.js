5008214190f8a9001f0f6e17fd8778993f7bc2cd
onmicanal <54179589+onmicanal@users.noreply.github.com>
2019-08-16 09:15:53 -0500

    new web page

---
+++ precache-manifest.e91ef0d01e5fc9dc4b1ae7740db971cc.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f22b9d05c2f1f230d24510313514c525",
+    "url": "/index.html"
+  },
+  {
+    "revision": "69382076afc277ca2142",
+    "url": "/static/css/2.3e372a93.chunk.css"
+  },
+  {
+    "revision": "43e83785961c49e07fac",
+    "url": "/static/css/main.2406c1bb.chunk.css"
+  },
+  {
+    "revision": "69382076afc277ca2142",
+    "url": "/static/js/2.c0fb45ab.chunk.js"
+  },
+  {
+    "revision": "43e83785961c49e07fac",
+    "url": "/static/js/main.3db6f950.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "6be73692b00cfe7f74bbd00a7f453b72",
+    "url": "/static/media/cajavoladora.6be73692.png"
+  },
+  {
+    "revision": "3a7a5829ab93b1f340963073d6b7de25",
+    "url": "/static/media/jamarLogo.3a7a5829.svg"
+  },
+  {
+    "revision": "0e9bb84e6aa28e6f7f554726008c6944",
+    "url": "/static/media/vendedoras.0e9bb84e.png"
+  }
+]);
